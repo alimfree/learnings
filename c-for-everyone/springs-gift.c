1075f196e4bca4ba4887d361a68a703bbb8c48dc
Mohammed Ali <mali@teladoc.com>
2021-12-19 18:23:23 -0600

    favorite poem peer assignment wk1"

---
+++ c-for-everyone/springs-gift.c
@@ -0,0 +1,63 @@
+/* Springs Gift Poem by Hamza Yusuf
+   Peer Graded Assignment
+
+   Mohammed Ali
+   Dec 19, 2021
+ 
+*/
+
+#include <stdio.h>
+int main(void) {
+  printf("\nSpring's Gift\n\n");
+  printf("I envy the sand that met his feet\n");
+  printf("I’m jealous of honey he tasted sweet\n");
+  printf("Of birds that hovered above his head\n");
+  printf("Of spiders who spun their sacred web\n");
+  printf("To save him from his enemies\n");
+  printf("I envy clouds formed from the seas\n");
+  printf("That gave him cover from the heat\n");
+  printf("Of a sun whose light could not compete\n");
+  printf("With his, whose face did shine so bright\n");
+  printf("That all was clear in blinding night\n");
+  printf("I envy sightless trees that gazed\n");
+  printf("Upon his form completely dazed\n");
+  printf("Not knowing if the sun had risen\n");
+  printf("But felt themselves in unison\n");
+  printf("With those who prayed, and fasted to\n");
+  printf("Simply because he told them to\n");
+  printf("With truth and kindness, charity\n");
+  printf("From God who gave such clarity\n");
+  printf("His mercy comes in one He sent\n");
+  printf("To mold our hearts more heaven bent\n");
+  printf("I envy all there at his side\n");
+  printf("Who watched the turning of the tide\n");
+  printf("As truth prevailed and falsehood fled\n");
+  printf("And hope restored life to the dead\n");
+  printf("Men and Women through him found grace\n");
+  printf("To seek together God’s noble face\n");
+  printf("I envy the cup that gave him drink\n");
+  printf("His thoughts that helped us all to think\n");
+  printf("To be one thought that passed his mind\n");
+  printf("Inspiring him to act so kind\n");
+  printf("For me this world is not one jot\n");
+  printf("If I could simply be a thought\n");
+  printf("From him to God throughout the ages\n");
+  printf("As revelation came in stages\n");
+  printf("I pity all who think it odd\n");
+  printf("To hear him say there is one God\n");
+  printf("Or he was sent by God to men\n");
+  printf("To hone their spirits’ acumen\n");
+  printf("It’s pride that blinds us from the sight\n");
+  printf("That helps good men to see his light\n");
+  printf("He taught us all to be God’s slaves\n");
+  printf("And he will be the one who saves\n");
+  printf("Humanity from sinful pride\n");
+  printf("Muhammad has God on his side\n");
+  printf("So on this day be blessed and sing\n");
+  printf("For he was born to grace our Spring\n");
+  printf("With lilies, flowers, life’s rebirth\n");
+  printf("In a dome of green like his on earth\n\n");
+  printf("-Sh. Hamza Yusuf\n\n");  
+
+  return 0; 
+} 
