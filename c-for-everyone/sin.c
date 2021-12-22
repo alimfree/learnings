023d9fdee77a3a1aab670db26160ee7bbbff2dff
Mohammed Ali <mali@teladoc.com>
2021-12-21 21:55:45 -0600

    refactored sin

--- c-for-everyone/sin.c
+++ c-for-everyone/sin.c
@@ -12,9 +12,13 @@
 int main(void)
 {
   double a; double sine_a;
-  printf("Enter a value between 0 and 1 (non inclusive)");
+  printf("\nEnter a value between 0 and 1 (non inclusive)\n");
   scanf("%lf", &a);
+  if(a <= 0 || a >= 1 ){
+    printf("input must be between 0 and 1 (non inclusive)\n\n");
+    return 0;
+  }
   sine_a = sin(a);
-  printf("The sine of %lf is: %lf\n", a, sine_a); 
+  printf("The sine of %lf is: %lf\n\n", a, sine_a); 
   return 0;
 }
