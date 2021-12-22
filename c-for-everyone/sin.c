07cdf7808d16d6d30ae91507e8c7b35bd1bfac0f
Mohammed Ali <mali@teladoc.com>
2021-12-21 23:26:15 -0600

    sin

--- c-for-everyone/sin.c
+++ c-for-everyone/sin.c
@@ -1,5 +1,5 @@
 /* Sin function
- * Write a program that prints the sine ofs a 
+ * Write a program that prints the sine of a 
  * number between 0 and 1 exclusive
  * 
  * Mohammed Ali
@@ -14,10 +14,13 @@ int main(void)
   double a; double sine_a;
   printf("\nEnter a value between 0 and 1 (non inclusive)\n");
   scanf("%lf", &a);
+
+  // End program if input is not between 0 and 1
   if(a <= 0 || a >= 1 ){
     printf("input must be between 0 and 1 (non inclusive)\n\n");
     return 0;
   }
+
   sine_a = sin(a);
   printf("The sine of %lf is: %lf\n\n", a, sine_a); 
   return 0;
